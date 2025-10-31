36513e9475eb1185ed256e0211539893b5a3d929
DiadkoPropan <arsenij.guba.2016@gmail.com>
2025-10-31 13:38:50 +0200

    Update note.js

--- src/models/note.js
+++ src/models/note.js
@@ -6,10 +6,12 @@ const noteSchema = new Schema(
   {
     title: {
       type: String,
+      trim: true,
       required: true,
     },
     content: {
       type: String,
+      trim: true,
       required: false,
       default: '',
     },
